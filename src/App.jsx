4758bad503c419737d14fd84c6b08781a0b89967
Recep Demir <recep.demir@powercoders.org>
2026-01-23 21:35:41 +0100

    feat: import HeroSection component in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { ThemeProvider } from './context/ThemeContext'
 import Navbar from './components/Navbar'
+import HeroSection from './components/HeroSection'
 
 const App = () => {
   return (
