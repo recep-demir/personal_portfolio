7575167e0b3238822a0a8fca867a193e21f9b959
Recep Demir <recep.demir@powercoders.org>
2026-02-16 21:53:59 +0100

    feat: implement SkillsSection component and update App structure; add project images

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import { ThemeProvider } from './context/ThemeContext'
 import Navbar from './components/Navbar'
 import HeroSection from './components/Section/HeroSection'
+import SkillsSection from './components/Section/SkillsSection'
 
 const App = () => {
   return (
     <ThemeProvider>
-      <div className='pb-96'>
+      <div className='pb-[100vh]' >
         <Navbar />
         <HeroSection />
-                <SkillSection />
+        <SkillsSection />
+
 
 
       </div>
